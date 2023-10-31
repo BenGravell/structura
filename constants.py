86569c0afa6aabe9dfec63bb442c8d1539d4643e
Ben <42848659+BenGravell@users.noreply.github.com>
2023-10-31 08:58:26 -0400

    Reduce draw update time

--- constants.py
+++ constants.py
@@ -3,7 +3,7 @@ import structure_utils
 
 # Minimum time interval between image draw updates.
 # Needs to be fairly high to work reliably in the deployed app, but can be zero when running locally.
-DRAW_UPDATE_SEC = 0.5
+DRAW_UPDATE_SEC = 0.3
 
 
 # TODO expose via app options
